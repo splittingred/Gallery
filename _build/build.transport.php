e3c6a0aa4c33e1ab979824abec7ae6b31b6608a7
Shaun McCormick <shaun@modx.com>
2010-12-20 13:10:49 -0600

    Added changelog to package

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -237,6 +237,7 @@ unset($vehicle,$menu);
 $builder->setPackageAttributes(array(
     'license' => file_get_contents($sources['docs'] . 'license.txt'),
     'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
+    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
     //'setup-options' => array(
         //'source' => $sources['build'].'setup.options.php',
     //),
